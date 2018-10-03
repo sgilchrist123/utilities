8099a809881789495b8f22d9c3ee5a1fc6164853
sgilchrist123 <sgilchrist123@gmail.com>
2018-10-03 17:30:03 +1000

    add and commit eslintrc

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ exports.getAttachmentLinks = (req, res) => {
     if(pageURL.indexOf(".pdf") >= (pageURL.length-4)) {
 		res.send(pageURL);
     } else {    
-    	var request = require('request');
-        	request(pageURL, function (error, response, body) {
+        var request = require('request');
+        request(pageURL, function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body.length); // Print the HTML for the Google homepage.
