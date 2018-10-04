90caeaf011b7a72e2febcd1cd26860d1be2c47f1
Simon <sgilchrist123@gmail.com>
2018-10-04 22:21:01 +1000

    Add tests and improve comments

--- test.js
+++ test.js
@@ -7,11 +7,24 @@ test('correct site URL is generated', () => {
 });
 
 
-
 test('correct greeting is generated', () => {
-  expect(index.isLinkToPDF("http://www.foo.com/report.pdf")).toBe(true);
-  //expect(index.isLinkToPDF("http://www.foo.com/report.PDF")).toBe(true);
   //expect(index.isLinkToPDF("http://www.foo.com/report.pdf?something=something")).toBe(true);
   expect(index.isLinkToPDF("http://www.foo.com/index.html")).toBe(false);
+}); 
+
+test('correct greeting is generated', () => {
   expect(index.isLinkToPDF("http://www.foo.com/pdfs/index.html")).toBe(false);
 });     
+
+test('isLinkToPDF - Link to PDF', () => {
+  expect(index.isLinkToPDF("http://www.foo.com/report.pdf")).toBe(true);
+  
+});  
+
+test('isLinkToPDF - Link to PDF - Uppercase', () => {
+  expect(index.isLinkToPDF("http://www.foo.com/report.PDF")).toBe(true);
+});
+
+test('isLinkToPDF - trim input', () => {
+  expect(index.isLinkToPDF(" http://www.foo.com/report.PDF ")).toBe(true);
+}); 
